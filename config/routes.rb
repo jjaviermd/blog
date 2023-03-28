296602619eb67c67f10d66410796457438281147
jjavier <razornice@gmail.com>
2023-03-28 16:42:00 -0300

    new and edit articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+  # say.. the homepage
 
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
